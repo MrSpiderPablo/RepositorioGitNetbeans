2086d1e820ff651b69a581116d090dc78b870d92
pablo <pabloramosblanquez@gmail.com>
2022-02-18 14:24:10 +0100

    Iniciando proyecto

--- src/main/java/Circulo/Circulo.java
+++ src/main/java/Circulo/Circulo.java
@@ -17,8 +17,15 @@ public class Circulo
         double r=s.nextDouble();
         double area=((Math.PI)*r*r);
         System.out.printf("El valor del area es %f\n", area);
+        System.out.printf("El valor de la longitud es %f\n", longitud(r));
      }catch(Exception e)
      {System.out.println("Error");
      }
+     
      }
+    public static double longitud(double radio)
+    {
+       double l= 2*(Math.PI)*radio;
+       return l;
+    }
     }
