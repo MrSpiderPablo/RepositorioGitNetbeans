6449aba205363e650fb3da58cc08b4269ab61a5c
pablo <pabloramosblanquez@gmail.com>
2022-02-18 14:07:09 +0100

    Iniciando proyecto

--- src/main/java/Circulo/Circulo.java
+++ src/main/java/Circulo/Circulo.java
@@ -10,10 +10,15 @@ public class Circulo
 {
     public static void main(String[]args)
     {
+     try
+     {
         Scanner s= new Scanner (System.in);
         System.out.println("introduce el radio: ");
         double r=s.nextDouble();
         double area=((Math.PI)*r*r);
         System.out.printf("El valor del area es %f\n", area);
+     }catch(Exception e)
+     {System.out.println("Error");
+     }
+     }
     }
-}
